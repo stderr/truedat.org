74dbec9e1d86e68d2a3aecad466c7e037370689d
Joshua Fenio <stderr@truedat.org>
2013-08-02 01:09:44 -0600

    Add initial templates and Bootstrap rc1.

--- truedat.py
+++ truedat.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+app.debug = True
 
 @app.route("/")
-def root():
-  return "truedat.org"
+def index():
+  return render_template("index.html")
 
 if __name__ == "__main__":
   app.run()
