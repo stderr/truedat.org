61aa9a014ba1daaa8a41a927eab16f8e9e729a37
Joshua Fenio <std3rr@gmail.com>
2013-08-03 15:43:23 -0600

    Prepare gitignore and templates for new app/services structure.

--- truedat.py
+++ truedat.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+from app.feeds import feeder
+
 app = Flask(__name__)
 app.debug = True
 
 @app.route("/")
 def index():
-  return render_template("index.html")
+  activities = feeder.latest()
+
+  return render_template("index.html", activities=activities)
 
 if __name__ == "__main__":
   app.run()
