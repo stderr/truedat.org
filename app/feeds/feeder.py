fc399a11b348372ea150c3166537351c3b01f6f7
Joshua Fenio <std3rr@gmail.com>
2013-08-05 11:03:44 -0600

    Quick, sensible refactor of naming conventions and dep injection.

--- app/feeds/feeder.py
+++ app/feeds/feeder.py
@@ -1,12 +1,16 @@
 import ConfigParser # I hate littering my environment with variables for configurable authentication, configs are easier for deployment and development purposes
-from services import Tweets, GitHub
+from services import Twitter, GitHub
 
 
-def latest():
+def latest(limit=7):
+  """
+    Controller interface to feed services. Definitely needs a makeover.
+  """
+
   config = ConfigParser.RawConfigParser()
   config.read('config/auth.cfg') # not ideal, but will fix soon
 
-  tweeter = Tweets({'consumer_secret': config.get('twitter', 'consumer_secret'),
+  tweeter = Twitter({'consumer_secret': config.get('twitter', 'consumer_secret'),
                   'access_token': config.get('twitter', 'access_token'),
                   'consumer_key': config.get('twitter', 'consumer_key'),
                   'access_secret': config.get('twitter', 'access_secret')})
@@ -14,7 +18,7 @@ def latest():
   tweets = tweeter.pull(config.get('twitter', 'shortname'))[:5]
 
   github = GitHub()
-  github_activity = github.pull('stderr')
+  github_events = github.pull('stderr')
 
-  merged = tweets + github_activity
-  return sorted(merged, key=lambda activity: activity.date, reverse=True)[:7]
+  merged = tweets + github_events
+  return sorted(merged, key=lambda activity: activity.date, reverse=True)[0:limit]
--- app/feeds/services.py
+++ app/feeds/services.py
@@ -16,6 +16,9 @@ def to_mst(date):
   return mst
 
 class Activity(object):
+  """
+    A wrapper for all activities or events from disparate services.
+  """
   def __init__(self, atype, title, message, date, url="#"):
     self.date = date 
 
@@ -27,7 +30,11 @@ class Activity(object):
   def __eq__(self, other):
     return self.date == other.date and self.title == other.title and self.message == other.message
 
-class Feed(object):
+class Service(object):
+  """
+    Superclass for all services.  self.date_format arguably should be in Activity, but I'm keeping it here for now.
+  """
+
   def __init__(self):
     self.activities = []
     self.date_format = "on %a %B %d, at %I:%M%p" 
@@ -35,12 +42,17 @@ class Feed(object):
   def _parse_service_timestamp(self, time):
     return datetime.strptime(time, self.service_time_format)
 
-class GitHub(Feed):
+class GitHub(Service):
+  """
+    GitHub service.  acceptable is a map of event types (as defined by GitHub) to title formats.
+    Probably should move the default definition elsewhere, but I like the dependency injection as it stands.
+  """
+
   def __init__(self, acceptable = {'PushEvent': "Pushed a total of %(commit_count)i commit%(pluralize)s to %(repo)s %(date)s"}):
     self.acceptable = acceptable
     self.activity_url = "https://api.github.com/users/%s/events"
     self.service_time_format = "%Y-%m-%dT%H:%M:%SZ" 
-    Feed.__init__(self)
+    Service.__init__(self)
 
   def pull(self, user):
     request_url = self.activity_url % user
@@ -64,7 +76,11 @@ class GitHub(Feed):
 
     return self.activities
 
-class Tweets(Feed):
+class Twitter(Service):
+  """
+    The service class for Twitter. Oh, OAuth, how I hate and love thee concurrently.
+  """
+
   def __init__(self, auth = {}):
     self.api = twitter.Api(
                            consumer_key = auth['consumer_key'],
@@ -74,7 +90,7 @@ class Tweets(Feed):
                            )
     self.service_time_format = "%a %b %d %H:%M:%S +0000 %Y" 
 
-    Feed.__init__(self)
+    Service.__init__(self)
 
   def pull(self, user):
     for a in self.api.GetUserTimeline(user):
